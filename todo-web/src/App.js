8308bb45442a9794bf0f75d150ba4116b38a830e
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2019-11-01 17:45:29 +0100

    Login form

---
+++ todo-web/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import LoginForm from './user/LoginForm';
+
+function App() {
+  return (
+    // <div>
+    //   Email: <input type="text"></input>
+    //   Password: <input type="password"></input>
+    // </div>
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-web/src/user/LoginForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+export default class LoginForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const value = event.target.value;
+        const property = event.target.id;
+
+        this.setState({
+            ...this.state,
+            [property]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log(`login form submitted. Email: ${this.state.email}, password: ${this.state.password}`);
+        event.preventDefault();
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Email: <input id='email' type="text" value={this.state.email} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Password: <input id='password' type="password" value={this.state.password} onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
